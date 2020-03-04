e7aea031395b143f23738f66efe1a3d1b9338568
robertjankowski <robjanw11@wp.pl>
2020-03-04 22:26:08 +0100

    measure time of functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "measure_time.h"
 #include "kmeans.h"
 
 int main()
@@ -6,7 +7,10 @@ int main()
     auto points = Observation::getData("../data/n=100000_c=4.csv");
 
     constexpr int k = 4;
-    const auto centroids = Kmeans::fit(points, k, 0.01, 100);
-    for (auto &c : centroids)
-        std::cout << c.getX() << " " << c.getY() << '\n';
+    // const auto centroids = Kmeans::fit(points, k, 0.01, 100);
+    // for (auto &c : centroids)
+    //     std::cout << c.getX() << " " << c.getY() << '\n';
+
+    auto elapsed_time = measure<milliseconds>::measure_time(Kmeans::fit, points, k, 0.01, 100).count();
+    std::cout << "Elapsed: " << elapsed_time  << " ms" << std::endl;
 }
