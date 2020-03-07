44c118adbaa72718723f18619d10734089c7a768
robertjankowski <robjanw11@wp.pl>
2020-03-07 20:59:18 +0100

    k [no. cluster] as parameter

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,23 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cerr << "Usage: " << argv[0] << " <data_filename>\n";
+        std::cerr << "Usage: " << argv[0] << " <data_filename> n_clusters\n";
         exit(1);
     }
     auto points = Observation::getData(argv[1]);
 
-    constexpr int k = 10;
+    int k = atoi(argv[2]);
     constexpr double tolerance = 0.001;
     constexpr int maxIteration = 1000;
 
-    // const auto centroids = Kmeans::fit(points, k, tolerance, maxIteration);
-    // for (auto &c : centroids)
-    //     std::cout << c.getX() << " " << c.getY() << '\n';
+    const auto centroids = Kmeans::fit(points, k, tolerance, maxIteration);
+    for (auto &c : centroids)
+        std::cout << c.getX() << " " << c.getY() << '\n';
 
-    auto elapsed_time = measure<milliseconds>::measure_time(Kmeans::fit, points, k, tolerance, maxIteration).count();
-    std::cout << "Elapsed: " << elapsed_time << " ms" << std::endl;
+    // auto elapsed_time = measure<milliseconds>::measure_time(Kmeans::fit, points, k, tolerance, maxIteration).count();
+    // std::cout << "Elapsed: " << elapsed_time << " ms" << std::endl;
 
     auto measureData = MeasureData{points, k, tolerance, maxIteration};
     // benchmarkSingle(std::move(measureData), 10, "../benchmarks/n=1000_c=2.dat");
