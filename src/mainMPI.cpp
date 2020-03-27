c4c4801b6bb2d052ae5c8986304bab9706496b2e
robertjankowski <robjanw11@wp.pl>
2020-03-27 21:23:16 +0100

    fix mainMPI

--- src/mainMPI.cpp
+++ src/mainMPI.cpp
@@ -7,22 +7,17 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        std::cerr << "Usage: " << argv[0] << " <data_filename>\n";
-        exit(1);
-    }
+    const auto fileWithK = Utils::loadFilenameWithKClusters(argc, argv);
+    const std::string inputFile = std::get<0>(fileWithK);
+    const int k = std::get<1>(fileWithK);
+    
+    constexpr double tolerance = 0.001;
+    constexpr int maxIteration = 1000;
+
     MPI_Init(NULL, NULL);
     int worldRank, worldSize;
     MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
-
-    const auto fileWithK = Utils::loadFilenameWithKClusters(argc, argv);
-    const std::string inputFile = std::get<0>(fileWithK);
-    const int k = std::get<1>(fileWithK);
-
-    constexpr double tolerance = 0.001;
-    constexpr int maxIteration = 1000;
 
     std::vector<Communication::CentroidsForWorker> allCentroids;
 
